ea4b42436a3f5aa73af439f925845f285d564de5
TheWaleedAfridi <148973441+TheWaleedAfridi@users.noreply.github.com>
2024-03-29 16:15:15 +0400

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,66 @@
+
+//ADD YOUR FIREBASE LINKS HERE
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD7e1R58gnu3G64Un9Yl9avayzOVJuc1OQ",
+  authDomain: "kwitter-3d9c9.firebaseapp.com",
+  projectId: "kwitter-3d9c9",
+  storageBucket: "kwitter-3d9c9.appspot.com",
+  messagingSenderId: "1026725246730",
+  appId: "1:1026725246730:web:ceb20f0c9fa78e68e9ecf3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+       Room_names = childKey;
+      //Start code
+      console.log("Room Name - " +Room_names);
+      row = "<div class='room_name' id="+Room_names+" onclick='redirectToRoomName(this.id)' >#"+ Room_names +"</div><hr>";
+      document.getElementById("output").innerHTML += row;
+
+
+      //End code
+      });});}
+getData();
+
+function addRoom()
+{
+      room_name = document.getElementById("room_name").value;
+      firebase.database().ref("/").child(room_name).update({purpose : "adding room name"});
+      localStorage.setItem("room_name", room_name);
+      window.location = "kwitter_page.html";
+}
+
+function redirectToRoomName(name)
+{
+      console.log(name);
+      localStorage.setItem("room_name", name);
+      window.location = "kwitter_page.html";
+}
+
+function logout()
+{
+      localStorage.removeItem("user_name");
+      localStorage.removeItem("room_name");
+      window.location = "kwitter.html";
+}
+
+function send()
+{
+      msg = document.getElementById("msg").value;
+      firebase.database().ref(room_name).push({
+            name:user_name,
+            message:msg,
+            like:0
+      });
+
+      document.getElementById("msg").value = "";
+}
